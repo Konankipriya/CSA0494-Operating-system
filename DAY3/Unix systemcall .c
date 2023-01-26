75ab097098841836f3a735f2f87d1ca06c0edacc
Konankipriya <112790443+Konankipriya@users.noreply.github.com>
2023-01-26 20:46:10 +0530

    Create Unix systemcall .c

---
+++ DAY3/Unix systemcall .c
@@ -0,0 +1,31 @@
+#include<stdio.h> 
+#include <fcntl.h> 
+
+int main() 
+{ 
+
+    int fd1 = open("foo.txt", O_RDONLY); 
+
+    if (fd1 < 0) 
+
+    { 
+
+        perror("c1"); 
+
+        exit(1); 
+
+    } 
+
+    printf("opened the fd = % d\n", fd1); 
+
+    if (close(fd1) < 0) 
+
+    { 
+
+        perror("c1"); 
+
+        exit(1); 
+
+    } 
+    printf("closed the fd.\n"); 
+}
